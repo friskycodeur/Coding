44e7c4d1a2130f2cfa196be3f9d40be2bc5b1350
Prateek Maheshwari <friskycodeur@gmail.com>
2021-04-09 22:37:20 +0530

    Arrays : 450 DSA questions

--- geekforgeeks/arrays/sumofelements.cpp
+++ geekforgeeks/arrays/sumofelements.cpp
@@ -1,5 +1,3 @@
-// Difficulty : Easy
-
 // { Driver Code Starts
 #include<bits/stdc++.h>
 using namespace std;
@@ -10,39 +8,39 @@ class Solution{
     public:
     // arr: input array
     // n: size of array
-    //Function to sort the array into a wave-like array.
-    void convertToWave(int *arr, int n){
-        for(int i=0;i<n;i+=2){
-            if(i!=n-1) swap(arr[i],arr[i+1]);
+    //Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(int arr[], int n){
+        int ans=arr[0];
+        int cur=arr[0];
+        for(int i=1;i<n;i++){
+            cur=max(arr[i],cur+arr[i]);
+            ans=max(ans,cur);
         }
+        return ans;
         
     }
 };
-
-void swap(int a,int b){
-    int temp=a;
-    a=b;
-    b=temp;
-}
 
 // { Driver Code Starts.
 
 int main()
 {
     int t,n;
-    cin>>t; //Input testcases
-    while(t--) //While testcases exist
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
     {
+        
         cin>>n; //input size of array
-        int a[n]; //declare array of size n
+        
+        int a[n];
+        
         for(int i=0;i<n;i++)
-            cin>>a[i]; //input elements of array
-        Solution ob;
-        ob.convertToWave(a, n);
-
-        for(int i=0;i<n;i++)
-            cout<<a[i]<<" "; //print array
+            cin>>a[i]; //inputting elements of array
             
-        cout<<endl;
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
     }
-}  // } Driver Code Ends
+}
+  // } Driver Code Ends
